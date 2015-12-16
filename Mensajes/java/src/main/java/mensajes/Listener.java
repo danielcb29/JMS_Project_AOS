d2577d00e988043528482d5efe79a865cd11ad54
cextreme <cextreme@alumnos.unex.es>
2015-12-16 19:20:34 +0100

    Ejemplo de envio de mensajes en java comentado y revisado

--- Mensajes/java/src/main/java/mensajes/Listener.java
+++ Mensajes/java/src/main/java/mensajes/Listener.java
@@ -1,69 +1,93 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Envio de mensajes con Java
+ * Daniel Correa, Cristina Extremera
  */
 package mensajes;
 
 import org.fusesource.stomp.jms.*;
 import javax.jms.*;
 
+/**
+ * Clase que recibe los mensajes
+ * @author Daniel Correa, Cristina Extremera
+ */
 class Listener {
 
     public static void main(String []args) throws JMSException {
 
+        //Configuracion inicial para la comunicacion
         String user = env("APOLLO_USER", "admin");
         String password = env("APOLLO_PASSWORD", "password");
         String host = env("APOLLO_HOST", "localhost");
         int port = Integer.parseInt(env("APOLLO_PORT", "61613"));
         String destination = arg(args, 0, "/topic/event");
 
+        //Creacion de fabrica de conexiones y seteo de variable URI del broker
         StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
         factory.setBrokerURI("tcp://" + host + ":" + port);
 
+        //Creacion de la conexion con el usuario y contraseña correspondiente       
         Connection connection = factory.createConnection(user, password);
+        //Inicio de la conexion
         connection.start();
+
+        //Creacion de la sesion
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination dest = new StompJmsDestination(destination);
 
+        //Se crea el consumidor de mensajes
         MessageConsumer consumer = session.createConsumer(dest);
+
+        //Obtenemos el tiempo actual para que cuando se termine la conexion sepamos cuanto tiempo ha durado esta
         long start = System.currentTimeMillis();
+        //Contador que iremos incrementando por cada mensaje recibido
         long count = 0;
+
+        //Se informa la espera de mensajes
         System.out.println("Estas listo para recibir mensajes!");
+
+        //Ciclo de espera de mensajes
         while(true) {
+
+            //Mensaje recibido
             Message msg = consumer.receive();
+
+            //Verificacion que es un mensaje de texto
             if( msg instanceof  TextMessage ) {
+                //Cuerpo del mensaje
                 String body = ((TextMessage) msg).getText();
+
+                //Si es shutdown acaba la comunicacion
                 if( "SHUTDOWN".equals(body)) {
                     long diff = System.currentTimeMillis() - start;
                     System.out.println(String.format("Recibidos %d mensajes en %.2f segundos", count, (1.0*diff/1000.0)));
                     break;
                 } else {
+                    //Caso contrario se imprime el mensaje recibido
                     count++;
-                    if( count == 0 ) {
+                    if( count == 0 ) { //Si es el primer mensaje que recibo, obtengo el tiempo actual
                         start = System.currentTimeMillis();
                     }
                     System.out.println("Has recibido: "+body);
                 }
 
             } else {
+                //Si no es una instancia de texto, es un mensaje inesperado
                 System.out.println("Unexpected message type: "+msg.getClass());
             }
         }
+
+        //Cierre de la conexion
         connection.close();
     }
 
+
+    /**
+     * Metodo auxiliar que permite saber si hay alguna variable en el ambito de trabajo con nombre key,
+     * en caso de no exisitir dicha variable se retorna defaultValue
+     * @param key: Nombre de la variable que esta en el ambiente
+     * @param defaultValue: Valor por defecto
+     */
     private static String env(String key, String defaultValue) {
         String rc = System.getenv(key);
         if( rc== null )
@@ -71,6 +95,13 @@ class Listener {
         return rc;
     }
 
+
+    /**
+     * Busca el argumento almacenado en la posicion index y en caso de no exisitir se retorna defaultValue
+     * @param args: argumentos de la aplicacion
+     * @param index: posicion del argumento
+     * @param defaultValue: valor por defecto
+     */
     private static String arg(String []args, int index, String defaultValue) {
         if( index < args.length )
             return args[index];
--- Mensajes/java/src/main/java/mensajes/Publisher.java
+++ Mensajes/java/src/main/java/mensajes/Publisher.java
@@ -1,18 +1,6 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Envio de mensajes con Java 
+ * Daniel Correa, Cristina Extremera
  */
 package mensajes;
 
@@ -22,43 +10,64 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-
+/**
+ * Clase encargada del envio de mensajes
+ * @author Daniel Correa, Cristina Extremera
+ */
 class Publisher {
 
     public static void main(String []args) throws JMSException {
 
+        //Configuracion inicial para la comunicacion 
         String user = env("APOLLO_USER", "admin");
         String password = env("APOLLO_PASSWORD", "password");
         String host = env("APOLLO_HOST", "localhost");
         int port = Integer.parseInt(env("APOLLO_PORT", "61613"));
         String destination = arg(args, 0, "/topic/event");
 
-        
-        
-
+        //Creacion de fabrica de conexiones y seteo de variable URI del broker
         StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
         factory.setBrokerURI("tcp://" + host + ":" + port);
 
+        //Creacion de la conexion con el usuario y contraseña correspondiente 
         Connection connection = factory.createConnection(user, password);
+        //Inicio de la conexion
         connection.start();
+
+        //Creacion de la sesion        
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination dest = new StompJmsDestination(destination);
+
+        //Se crea el productor de mensajes
         MessageProducer producer = session.createProducer(dest);
+        //Se inicializa el modo de no persistencia
         producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
         
+        //Se informa de que el envio de mensajes esta listo
         System.out.println("El envio de mensajes a iniciado!, digita el mensaje y pulsa enter");
+
+        //Ciclo de envio de mensajes
         while(true){
+
+            //Variables para realizar la lectura desde el teclado
         	String body="";
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+
+
             try{
+                //Lectura del mensaje por teclado
                 body=in.readLine();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             
+            //Creacion del mensaje
             TextMessage msg = session.createTextMessage(body);
+
+            //Envio del mensaje
             producer.send(msg);
+
+            //En el caso de que el mensaje fuese SHUTDOWN se cierra la comunicacion
             if (body.equals("SHUTDOWN")){
             	connection.close();
             	break;
@@ -70,6 +79,13 @@ class Publisher {
 
     }
 
+
+    /**
+     * Metodo auxiliar que permite saber si hay alguna variable en el ambito de trabajo con nombre key,
+     * en caso de no exisitir dicha variable se retorna defaultValue
+     * @param key: Nombre de la variable que esta en el ambiente
+     * @param defaultValue: Valor por defecto
+     */
     private static String env(String key, String defaultValue) {
         String rc = System.getenv(key);
         if( rc== null )
@@ -77,6 +93,13 @@ class Publisher {
         return rc;
     }
 
+
+    /**
+     * Busca el argumento almacenado en la posicion index y en caso de no exisitir se retorna defaultValue
+     * @param args: argumentos de la aplicacion
+     * @param index: posicion del argumento
+     * @param defaultValue: valor por defecto
+     */
     private static String arg(String []args, int index, String defaultValue) {
         if( index < args.length )
             return args[index];
