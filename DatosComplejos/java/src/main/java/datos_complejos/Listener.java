39201bbedd2d8ef91ca8d02ba0f3483e755d82d6
Daniel Correa <danielc.web@gmail.com>
2015-12-16 20:12:48 +0100

    Listener de datos complejos documentado

--- DatosComplejos/java/src/main/java/datos_complejos/Listener.java
+++ DatosComplejos/java/src/main/java/datos_complejos/Listener.java
@@ -51,31 +51,44 @@ class Listener {
             
             //Verificacion que es un mensaje de tipo ObjectMessage
             if( msg instanceof  ObjectMessage ) {
-            	
+            	//Se verifica que el objeto es de tipo estudiante
             	if(((ObjectMessage) msg).getObject() instanceof Estudiante){
+            		//se obtiene el objeto recibido
             		Estudiante body = (Estudiante)((ObjectMessage) msg).getObject();
+            		//se imprime la version texto del objeto
             		System.out.println(body.toString());
             	}
 
 
             } else {
+            	//si no es de tipo objeto se verifica que sea de texto 
             	if(msg instanceof  TextMessage){
+            		//se obtiene la cadena de texto
             		String body = ((TextMessage) msg).getText();
                     //Si es shutdown acaba la comunicacion
                     if( "SHUTDOWN".equals(body)) {
+                    	//se verifica que sea shutdown para acabar la comunicacion
                         long diff = System.currentTimeMillis() - start;
                         System.out.println(String.format("Recibidos %d mensajes en %.2f segundos", count, (1.0*diff/1000.0)));
                         break;
                     }
             	}else{
+            		//Si no es una instancia de texto, es un mensaje inesperado
             		System.out.println("Unexpected message type: "+msg.getClass());
             	}
                 
             }
         }
+        //Cierre de la conexion
         connection.close();
     }
-
+    
+    /**
+     * Metodo auxiliar que permite saber si hay alguna variable en el ambito de trabajo con nombre key,
+     * en caso de no exisitir dicha variable se retorna defaultValue
+     * @param key: Nombre de la variable que esta en el ambiente
+     * @param defaultValue: Valor por defecto
+     */
     private static String env(String key, String defaultValue) {
         String rc = System.getenv(key);
         if( rc== null )
@@ -83,6 +96,12 @@ class Listener {
         return rc;
     }
 
+    /**
+     * Busca el argumento almacenado en la posicion index y en caso de no exisitir se retorna defaultValue
+     * @param args: argumentos de la aplicacion
+     * @param index: posicion del argumento
+     * @param defaultValue: valor por defecto
+     */
     private static String arg(String []args, int index, String defaultValue) {
         if( index < args.length )
             return args[index];
